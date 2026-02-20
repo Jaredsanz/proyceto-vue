19ce73a549b3495da34bc8ebb96dee489d33a0f1
Jared <jesusjaredronal@gmail.com>
2026-02-20 01:36:22 -0600

    vue router

---
+++ vue-project/src/components/sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="sidebar">
+        <h2> Barra Lateral</h2>
+        <ul> 
+        <li><router-link to ="/contador">Contador </router-link></li>
+        <li><router-link to ="/Lista-de-tareas">Lista de tareas</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar{
+width: 200px;    
+background-color: rgb(233, 233, 238);
+padding:20px;
+}
+</style>
---
+++ vue-project/src/views/MainView.vue
@@ -0,0 +1,25 @@
+<template>  
+<div class ='container'>
+    <sidebar></sidebar>
+<div class ='content'>
+    <RouterView></RouterView>
+</div>
+
+</div>
+</template>
+
+<script setup>
+import sidebar from '@/components/sidebar.vue';
+</script>
+
+<style>
+.container {
+
+display: flex ;
+}
+.content {
+    flex: 1;
+    padding: 20px;
+}
+
+</style>
